9743a999ad90ad2c53491e92b4e194b135ea378b
Mahpara <mah4.para@gmail.com>
2025-10-03 11:39:04 +0100

    chore: added footer with tailwind styles

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+    <footer className="bg-purple-50 p-6 shadow-inner mt-12 text-center text-gray-600 text-sm">
+        <p>© 2025 How-To Hub, Made by Mahpara, with ❤️ for learners.</p>
+    </footer>
+    );
+}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-    <header className="bg-[#f7f0ff] p-4 shadow-md max-w-5xl mx-auto flex flex-col sm:flex-row items-center justify-between space-y-2 sm:space-y-0">
+    <header className="bg-purple-50 p-4 shadow-md max-w-5xl mx-auto flex flex-col sm:flex-row items-center justify-between space-y-2 sm:space-y-0">
     
         <h1 className="text-2xl font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent">
         How-To-Hub
