73aeb1643290287a5a5984b4204c780299d869c5
Mahpara <mah4.para@gmail.com>
2025-10-03 10:27:35 +0100

    chore: header with tailwind css, just wanted to see the color I want

---
+++ src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+    <header className="bg-[#f7f0ff] p-4 shadow-md max-w-5xl mx-auto flex flex-col sm:flex-row items-center justify-between space-y-2 sm:space-y-0">
+    
+        <h1 className="text-2xl font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent">
+        How-To-Hub
+        </h1>
+
+        <nav className="flex space-x-6">
+        <Link
+            href="/"
+            className="text-lg font-medium text-indigo-600 hover:text-[#f7f0ff] transition-colors"
+        >
+            Home
+        </Link>
+        <Link
+            href="/posts"
+            className="text-lg font-medium text-indigo-600 hover:text-[#f7f0ff] transition-colors"
+        >
+            Posts
+        </Link>
+        </nav>
+    </header>
+    );
+}
